8a240a4f41ed5cb1f32a4f21839e1414ea9482aa
Buk Bukowski <pythoncontrol@gmail.com>
2021-03-22 17:06:49 +0100

    fetcher\git\git.go: create basic implementation

--- fetcher/git/git.go
+++ fetcher/git/git.go
@@ -1,10 +1,45 @@
 package git
 
-import "github.com/bukowa/pkgo/src"
+import (
+	"github.com/bukowa/pkgo/fetcher"
+	"github.com/bukowa/pkgo/src"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
 
 type Fetcher struct {
+
 }
 
 func (f Fetcher) Fetch(p src.Package) (string, error) {
-	return "git", nil
+	p.WithFields().Infof("fetching")
+	dir, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", err
+	}
+	cmd := fetcher.NewCommandWithArgs("git", dir, "-C", dir)
+	args := [][]string{
+		{"clone", p.Source, "."},
+		{"fetch", "--recurse-submodules", "--depth=1", "origin", p.Version},
+		{"checkout", "FETCH_HEAD"},
+		{"submodule", "sync", "--recursive"},
+		{"submodule", "update", "--init", "--recursive", "--remote", "--depth=1", "--no-single-branch"},
+	}
+	return dir, gits(args, cmd)
+}
+
+func gits(args[][]string, f func(a ...string) ([]byte, error)) error {
+	for _, arg := range args {
+		if err := git(f(arg...)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func git(b []byte, err error) error {
+	if err != nil {
+		log.Print(string(b))
+	}
+	return err
 }
